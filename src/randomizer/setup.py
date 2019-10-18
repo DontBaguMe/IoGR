2763d9b724cb00aceabb54bde1943f5c58fa8b14
DontBaguMe <sscott7384@gmail.com>
2019-10-18 07:10:34 -0700

    Version 2.4.7

--- src/randomizer/setup.py
+++ src/randomizer/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='iog-randomizer',
-    version='2.4.6',
+    version='2.4.7',
     description='The Illusion of Gaia Randomizer',
     author='dontbagume,bryon_w,raeven0',
     packages=setuptools.find_packages(),
