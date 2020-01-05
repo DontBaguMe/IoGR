36de9af937247b1652477f1d1e46739ff0754933
DontBaguMe <49798147+DontBaguMe@users.noreply.github.com>
2020-01-05 13:32:47 -0800

    v2.8.3
    Further restrictions on Great Wall map 135

--- src/randomizer/randomizer/iogr_rom.py
+++ src/randomizer/randomizer/iogr_rom.py
@@ -13,7 +13,7 @@ from .models.enums.logic import Logic
 from .models.enums.enemizer import Enemizer
 from .models.enums.start_location import StartLocation
 
-VERSION = "2.8.2"
+VERSION = "2.8.3"
 
 KARA_EDWARDS = 1
 KARA_MINE = 2
