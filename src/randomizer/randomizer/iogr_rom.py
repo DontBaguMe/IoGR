3417cdb0ff6ff09b87b6ba129dd7b4c9331229ea
DontBaguMe <49798147+DontBaguMe@users.noreply.github.com>
2020-01-25 20:07:05 -0800

    v2.9.0

--- src/randomizer/randomizer/iogr_rom.py
+++ src/randomizer/randomizer/iogr_rom.py
@@ -13,7 +13,7 @@ from .models.enums.logic import Logic
 from .models.enums.enemizer import Enemizer
 from .models.enums.start_location import StartLocation
 
-VERSION = "2.8.7"
+VERSION = "2.9.0"
 
 KARA_EDWARDS = 1
 KARA_MINE = 2
