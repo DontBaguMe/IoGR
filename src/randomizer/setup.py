92d18b901c112c2b6021488608d1172a8a561e01
Seth Scott <sscott@fresnocog.org>
2020-06-15 11:43:46 -0700

    v3.2.1

--- src/randomizer/setup.py
+++ src/randomizer/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='iog-randomizer',
-    version='3.2.0',
+    version='3.2.1',
     description='The Illusion of Gaia Randomizer',
     author='dontbagume,bryon_w,raeven0',
     packages=setuptools.find_packages(),
