bca5ad2e3cc838d1e7d436427e6d57e2bb12f181
DontBaguMe <49798147+DontBaguMe@users.noreply.github.com>
2020-11-24 11:30:10 -0800

    New version

--- src/randomizer/randomizer/iogr_rom.py
+++ src/randomizer/randomizer/iogr_rom.py
@@ -14,7 +14,7 @@ from .models.enums.entrance_shuffle import EntranceShuffle
 from .models.enums.enemizer import Enemizer
 from .models.enums.start_location import StartLocation
 
-VERSION = "4.0.0"
+VERSION = "4.0.1"
 
 KARA_EDWARDS = 1
 KARA_MINE = 2
