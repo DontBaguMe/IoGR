0ff84227ec687de8514af5769e7ef442289fdb7d
Seth Scott <49798147+DontBaguMe@users.noreply.github.com>
2021-07-29 12:21:03 -0700

    v4.4.3

--- src/randomizer/randomizer/iogr_rom.py
+++ src/randomizer/randomizer/iogr_rom.py
@@ -15,7 +15,7 @@ from .models.enums.entrance_shuffle import EntranceShuffle
 from .models.enums.enemizer import Enemizer
 from .models.enums.start_location import StartLocation
 
-VERSION = "4.4.2"
+VERSION = "4.4.3"
 
 MAX_RANDO_RETRIES = 9
 PRINT_LOG = False
